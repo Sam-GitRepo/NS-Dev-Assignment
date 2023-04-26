315d1f2cd8e4c59e041a9d4d47c59942de9684d1
Sam <smarttsamy@gmail.com>
2023-04-26 08:01:39 +0530

    JS_Primitive_Types

---
+++ JS/JS_Primitive_Types.js
@@ -0,0 +1,10 @@
+// declare your variables below
+var varA = 28;
+var varB = 'String';
+var varC = false;
+var varD = undefined;
+var varE = {
+    name: 'Sam',
+    age: 24
+}
+var varF = Symbol('Symbol')
