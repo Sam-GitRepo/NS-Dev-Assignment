aae1cc97e480fe023d8794e437810745e2ab8199
Sam <smarttsamy@gmail.com>
2023-04-29 11:35:06 +0530

    JavaScript Recursion

---
+++ JS/Javascript_recursion.js
@@ -0,0 +1,13 @@
+const getValue = (num) => {
+    if (num < 10) {
+    return num;
+  }
+  let Sum = 0;
+  while (num > 0) {
+    Sum += num % 10;
+    num = Math.floor(num / 10);
+  }
+
+
+    return getValue(Sum);
+};
