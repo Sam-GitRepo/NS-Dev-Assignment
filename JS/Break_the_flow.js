8f15924272c72ea6bd8b859704422d3a9b055ad1
Sam <smarttsamy@gmail.com>
2023-05-20 08:38:03 +0530

    Break_the_flow

---
+++ JS/Break_the_flow.js
@@ -0,0 +1,19 @@
+const submitBtn = document.getElementById("submit-btn");
+// write your code here
+const form = document.querySelector('form');
+form.addEventListener('click',(event)=>{
+    event.preventDefault();
+});
+const firstDiv = document.querySelector(".first-div");
+const secondDiv = document.querySelector(".second-div");
+let output = document.querySelector(".output");
+
+firstDiv.addEventListener("click", () =>{
+    output.innerText += "first div called";
+});
+
+secondDiv.addEventListener("click", () => {
+    // write your code here
+    event.stopPropagation();
+    output.innerText += "second div called -- ";
+});
